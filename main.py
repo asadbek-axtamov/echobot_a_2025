9407a774ed96a5fd840af72ffef590387e128ab7
Asadbek <asadbekakhtamov20@gmail.com>
2025-01-14 17:20:27 +0500

    echo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import requests
 import os
 TOKEN = os.getenv("TOKEN")
+import time
 
-
+url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
 
 def get_updates():
 
@@ -15,6 +16,23 @@ def send_message(text, chat_id):
     r=requests.get(url)
     return r.json()
 
-CHAT_ID = "86775091"
-send_message("SALOM", CHAT_ID)
+CHAT_ID = 7157297566
+idx = -1
+while True:
+    response=requests.get(url)
+    data=response.json()
+    result=data["result"]
+    last_update=result[-1]
+    
+    last_update_id = last_update['update_id']
+    
+    if last_update_id != idx:
+        text=last_update["message"]["text"]
+        chat_id=last_update["message"]["chat"]["id"]
+        send_message(text,chat_id)
+        print(last_update_id, idx)
+    idx = last_update_id
+    time.sleep(0.5)
+
+    
 
